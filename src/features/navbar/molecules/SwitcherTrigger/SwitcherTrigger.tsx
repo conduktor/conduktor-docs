01748edd6e9f8c4a0224c2b6dbd7bb74ae539a16
Adelin Danaila <contact@adelindanaila.com>
2022-11-03 20:27:46 +0200

    fix: switcher names

--- src/features/navbar/molecules/SwitcherTrigger/SwitcherTrigger.tsx
+++ src/features/navbar/molecules/SwitcherTrigger/SwitcherTrigger.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from '@docusaurus/router'
 import useIsBrowser from '@docusaurus/useIsBrowser'
+import { items as ProductsItems } from '@site/src/features/home/organisms/Products/Products.constants'
 import React from 'react'
 import { StyledSwitcherTrigger, Trigger, TriggerIcon } from './SwitcherTrigger.styled'
 
@@ -7,16 +8,25 @@ interface SwitcherTriggerProps {
   onClick: () => void
 }
 
+const getPathName = (pathname: string) => {
+  const paths = ProductsItems.reduce(function (a, b) {
+    return a + ['', ', '][+!!a.length] + b.to
+  }, '')
+    .split(',')
+    .map(part => part.trim())
+  const parts = pathname.split('/')
+  const path = `/${parts[1]}/${parts[2]}`
+
+  if (paths.includes(path)) return parts[2].replace('-', ' ')
+  else if (parts[1] === 'desktop') return 'desktop'
+
+  return 'platform'
+}
+
 const SwitcherTrigger: React.FunctionComponent<SwitcherTriggerProps> = ({ onClick }) => {
   const location = useLocation()
   const isBrowser = useIsBrowser()
-  const pathname = isBrowser
-    ? location.pathname.split('/')[2]
-      ? location.pathname.split('/')[2]
-      : location.pathname.split('/')[1]
-      ? location.pathname.split('/')[1]
-      : 'platform'
-    : undefined
+  const pathname = isBrowser ? getPathName(location.pathname) : 'platform'
 
   return (
     <StyledSwitcherTrigger>
