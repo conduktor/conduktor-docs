9cf7df16006a085bfe445b4a4eba5fb0587b2863
Adelin Danaila <contact@adelindanaila.com>
2022-11-03 14:16:25 +0200

    feat: home products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from '@theme/Layout'
 import React from 'react'
 import Hero from '../features/home/organisms/Hero'
+import Products from '../features/home/organisms/Products'
 import useFluidContainer from '../hooks/useFluidContainer'
 
 export default function Home(): JSX.Element {
@@ -9,6 +10,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout>
       <Hero />
+      <Products />
     </Layout>
   )
 }
