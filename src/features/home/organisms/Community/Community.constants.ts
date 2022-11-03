16a5229f6925406f460b1973f549930143f721da
Adelin Danaila <contact@adelindanaila.com>
2022-11-03 15:48:20 +0200

    feat: home community

---
+++ src/features/home/organisms/Community/Community.constants.ts
@@ -0,0 +1,16 @@
+export const items = [
+  {
+    title: 'GitHub',
+    description:
+      'Follow along and contribute to the development of Conduktor,  and get help with GitHub discussions.',
+    icon: '/assets/svgs/community/github.svg',
+    href: 'https://github.com/conduktor',
+  },
+  {
+    title: 'Discord',
+    description:
+      'Join our awesome community of developers on Discord o help and show off what you built',
+    icon: '/assets/svgs/community/discord.svg',
+    href: '/',
+  },
+]
