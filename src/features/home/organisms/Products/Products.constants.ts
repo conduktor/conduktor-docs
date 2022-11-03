09a80653106223bcf8a8affa068ac61fe4fc2ce0
Adelin Danaila <contact@adelindanaila.com>
2022-11-03 14:16:25 +0200

    feat: home products

---
+++ src/features/home/organisms/Products/Products.constants.ts
@@ -0,0 +1,37 @@
+export const items = [
+  {
+    name: 'Console',
+    description: 'Explore and manage Kafka',
+    to: '/',
+    icon: '/assets/svgs/products/console.svg',
+    comingSoon: false,
+  },
+  {
+    name: 'Testing',
+    description: 'Automated Kafka testing',
+    to: '/',
+    icon: '/assets/svgs/products/testing.svg',
+    comingSoon: false,
+  },
+  {
+    name: 'Monitoring',
+    description: 'Tailored Kafka monitoring',
+    to: '/',
+    icon: '/assets/svgs/products/monitoring.svg',
+    comingSoon: false,
+  },
+  {
+    name: 'Data Masking',
+    description: 'Hide sensitive data',
+    to: '/',
+    icon: '/assets/svgs/products/dataMasking.svg',
+    comingSoon: true,
+  },
+  {
+    name: 'Topic as a Service',
+    description: 'Standardize your processes',
+    to: '/',
+    icon: '/assets/svgs/products/taas.svg',
+    comingSoon: true,
+  },
+]
---
+++ src/features/home/organisms/Products/Products.tsx
@@ -0,0 +1,41 @@
+import Container from '@site/src/components/atoms/Container'
+import React from 'react'
+import { items } from './Products.constants'
+import {
+  Heading,
+  List,
+  ListItem,
+  ListItemDescription,
+  ListItemLink,
+  ListItemName,
+  ListVisual,
+  ListVisualImage,
+  StyledProducts,
+} from './Products.styled'
+
+interface ProductsProps {}
+
+const Products: React.FunctionComponent<ProductsProps> = () => {
+  return (
+    <StyledProducts>
+      <Container>
+        <Heading>Browse by product</Heading>
+        <List>
+          {items.map((item, itemIndex) => (
+            <ListItem key={itemIndex}>
+              <ListItemLink to={item.to}>
+                <ListVisual>
+                  <ListVisualImage src={item.icon} alt={item.name} />
+                </ListVisual>
+                <ListItemName>{item.name}</ListItemName>
+                <ListItemDescription>{item.description}</ListItemDescription>
+              </ListItemLink>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
+    </StyledProducts>
+  )
+}
+
+export default Products
