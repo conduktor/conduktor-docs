ab7f14c5a586c3a2e35033d266d1c6c2ad755d99
Adelin Danaila <contact@adelindanaila.com>
2022-11-21 12:48:29 +0200

    style: mobile responsive

--- src/theme/Footer/Copyright/index.js
+++ src/theme/Footer/Copyright/index.js
@@ -1,15 +1,15 @@
-import Flex from '@site/src/components/atoms/Flex'
 import SocialIcons from '@site/src/features/footer/molecules/SocialIcons'
 import Copyright from '@theme-original/Footer/Copyright'
 import React from 'react'
+import styles from './copyright.module.scss'
 
 export default function CopyrightWrapper(props) {
   return (
     <>
-      <Flex align="center" justify="space-between">
+      <div className={styles.container}>
         <Copyright {...props} />
         <SocialIcons />
-      </Flex>
+      </div>
     </>
   )
 }
--- src/theme/TOCItems/Tree.js
+++ src/theme/TOCItems/Tree.js
@@ -1,10 +1,11 @@
 import Tree from '@theme-original/TOCItems/Tree'
 import React from 'react'
+import styles from './tree.module.scss'
 
 export default function TreeWrapper(props) {
   return (
     <>
-      <strong data-title>Quick nav</strong>
+      <strong className={styles.title}>Quick nav</strong>
       <Tree {...props} />
     </>
   )
