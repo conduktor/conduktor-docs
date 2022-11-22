a236a9817438e77dda3f416a9909acd8975ccab5
Adelin Danaila <contact@adelindanaila.com>
2022-11-22 13:21:07 +0200

    style: fix sticky header

--- src/features/doc/atoms/Heading/Heading.tsx
+++ src/features/doc/atoms/Heading/Heading.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 interface HeadingProps {}
 
+const STICKY_OFFSET = 100
 const Heading: React.FunctionComponent<HeadingProps> = () => {
   const [sticky, setSticky] = React.useState<boolean>(false)
 
   React.useEffect(() => {
+    if (window.scrollY >= STICKY_OFFSET) setSticky(true)
+
     const scrollEvent = () => {
-      setSticky(window.scrollY >= 100)
+      setSticky(window.scrollY >= STICKY_OFFSET ? true : false)
     }
     document.addEventListener('scroll', scrollEvent)
 
@@ -20,6 +23,17 @@ const Heading: React.FunctionComponent<HeadingProps> = () => {
     const headingElement = document.querySelector('main .markdown h1')
     if (headingElement) headingElement.setAttribute('data-sticky', sticky.toString())
   }, [sticky])
+
+  React.useEffect(() => {
+    const headingElement = document.querySelector('main .markdown h1')
+    if (headingElement) {
+      if (window.scrollY >= STICKY_OFFSET) headingElement.setAttribute('data-transition', 'false')
+
+      setTimeout(() => {
+        headingElement.setAttribute('data-transition', 'true')
+      }, 200)
+    }
+  }, [])
 
   return null
 }
