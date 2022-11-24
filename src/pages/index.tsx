d7283a147a8bb501a8db7f04f2e3e73611d6a07c
Adelin Danaila <contact@adelindanaila.com>
2022-11-24 17:07:09 +0200

    feat: home page meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import Head from '@docusaurus/Head'
 import Layout from '@theme/Layout'
-import React from 'react'
+import React, { Fragment } from 'react'
 import Community from '../features/home/organisms/Community'
 import GetStarted from '../features/home/organisms/GetStarted'
 import Guides from '../features/home/organisms/Guides'
@@ -7,16 +8,29 @@ import Hero from '../features/home/organisms/Hero'
 import Products from '../features/home/organisms/Products'
 import useFluidContainer from '../hooks/useFluidContainer'
 
+const META_DESCRIPTION =
+  'Find Documentation and FAQs for all Conduktor products, along with a weekly changelog and our Platform roadmap.'
+
 export default function Home(): JSX.Element {
   useFluidContainer()
 
   return (
-    <Layout>
-      <Hero />
-      <Products />
-      <GetStarted />
-      <Guides />
-      <Community />
-    </Layout>
+    <Fragment>
+      <Head>
+        <title>Conduktor Documentation</title>
+        <meta property="og:title" content="Conduktor Documentation" />
+        <meta property="og:description" content={META_DESCRIPTION} />
+        <meta name="twitter:description" content={META_DESCRIPTION} />
+        <meta name="description" content={META_DESCRIPTION} />
+      </Head>
+
+      <Layout>
+        <Hero />
+        <Products />
+        <GetStarted />
+        <Guides />
+        <Community />
+      </Layout>
+    </Fragment>
   )
 }
