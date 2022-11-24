5f1d669a7142d38c501aa490de468c7687a1f068
Adelin Danaila <contact@adelindanaila.com>
2022-11-24 16:27:24 +0200

    feat: open graph image

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,3 +1,4 @@
+import Head from '@docusaurus/Head'
 import Layout from '@theme-original/Layout'
 import React from 'react'
 import { IntercomProvider } from 'react-use-intercom'
@@ -5,6 +6,9 @@ import { IntercomProvider } from 'react-use-intercom'
 export default function LayoutWrapper(props) {
   return (
     <>
+      <Head>
+        <meta property="og:image" content="/assets/images/og.png" />
+      </Head>
       <IntercomProvider appId={process.env.REACT_APP_INTERCOM_ID} autoBoot={true}>
         <Layout {...props} />
       </IntercomProvider>
