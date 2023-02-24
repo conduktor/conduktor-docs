6bef4e1b0b8982ebb3b4175b730b9784d1a8c3b8
Adelin Danaila <contact@adelindanaila.com>
2023-02-24 14:57:12 +0200

    fix: quick nav state

--- src/theme/TOCItems/Tree.hooks.ts
+++ src/theme/TOCItems/Tree.hooks.ts
@@ -1,5 +1,4 @@
 import { useLocation } from '@docusaurus/router'
-import useIsBrowser from '@docusaurus/useIsBrowser'
 import { useEffect } from 'react'
 import { isMobile } from 'react-device-detect'
 import { hiddenByDefaultList } from './Tree.constants'
@@ -9,35 +8,38 @@ export const useTogglerState = ref => {
   const isEnabled = useQuickNavStore(state => state.state)
 
   useEffect(() => {
-    if (!ref.current) return
-
-    const element = ref.current
-    const parentElement = element.parentElement.parentElement
-    const parentElementSibling = parentElement.previousElementSibling
-
-    if (parentElement) {
-      if (isEnabled) {
-        setTimeout(() => {
-          parentElement.style.display = 'block'
-        }, 200)
-      } else parentElement.style.display = 'none'
-    }
-    if (parentElementSibling)
-      parentElementSibling.setAttribute(
-        'style',
-        `max-width: ${
-          isEnabled ? (isMobile ? 100 : 75) : 100
-        }% !important; transition: max-width 200ms ease-in-out;`
-      )
+    useQuickNavState(ref, isEnabled)
   }, [isEnabled])
 }
 
-export const usePresetStates = () => {
-  const isBrowser = useIsBrowser()
+export const usePresetStates = ref => {
   const location = useLocation()
   const setQuickNavState = useQuickNavStore(state => state.setState)
 
   useEffect(() => {
-    isBrowser && setQuickNavState(!hiddenByDefaultList.includes(location.pathname))
+    const state = !hiddenByDefaultList.includes(location.pathname)
+    setQuickNavState(state)
+    useQuickNavState(ref, state)
   }, [location.pathname])
+}
+
+export const useQuickNavState = (ref, isEnabled) => {
+  if (!ref.current) return
+
+  const element = ref.current
+  const parentElement = element.parentElement.parentElement
+  const parentElementSibling = parentElement.previousElementSibling
+
+  if (parentElement) {
+    if (isEnabled) {
+      parentElement.style.display = 'block'
+    } else parentElement.style.display = 'none'
+  }
+  if (parentElementSibling)
+    parentElementSibling.setAttribute(
+      'style',
+      `max-width: ${
+        isEnabled ? (isMobile ? 100 : 75) : 100
+      }% !important; transition: max-width 200ms ease-in-out;`
+    )
 }
