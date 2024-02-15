738a007a580fcf91c4bf55bb5da0bb258e174f99
Claudiu Sergiu Danaila <csdmedianet@gmail.com>
2024-02-15 18:01:14 +0000

    fonts, colours & adjustments
    To run some tests before merging.

--- src/styles/globalCss/fonts.ts
+++ src/styles/globalCss/fonts.ts
@@ -1,23 +1,23 @@
 import { globalCss } from '..'
 
-const Inter = (name: string, fontWeight: number, fontStyle = 'normal') => ({
-  fontFamily: 'Inter',
+const Roobert = (name: string, fontWeight: number, fontStyle = 'normal') => ({
+  fontFamily: 'Roobert',
   fontDisplay: 'swap' as any,
   fontStyle,
   fontWeight,
   src: `url('/fonts/${name}.woff2?v=3.19') format('woff2'), url('/fonts/${name}.woff?v=3.19') format('woff')`,
 })
 
-const PolySans = (name: string, fontWeight: number, fontStyle = 'normal') => ({
-  fontFamily: 'PolySans',
-  fontDisplay: 'swap' as any,
-  fontStyle,
-  fontWeight,
-  src: `url('/fonts/${name}.woff2') format('woff2'), url('/fonts/${name}.woff') format('woff')`,
-})
+// const Roobert = (name: string, fontWeight: number, fontStyle = 'normal') => ({
+//   fontFamily: 'Roobert',
+//   fontDisplay: 'swap' as any,
+//   fontStyle,
+//   fontWeight,
+//   src: `url('/fonts/${name}.woff2') format('woff2'), url('/fonts/${name}.woff') format('woff')`,
+// })
 
-const JetBrains = (name: string, fontWeight: number, fontStyle = 'normal') => ({
-  fontFamily: 'JetBrains Mono',
+const GeistMono = (name: string, fontWeight: number, fontStyle = 'normal') => ({
+  fontFamily: 'Geist Mono',
   fontDisplay: 'swap' as any,
   fontStyle,
   fontWeight,
@@ -26,23 +26,16 @@ const JetBrains = (name: string, fontWeight: number, fontStyle = 'normal') => ({
 
 export default globalCss({
   '@font-face': [
-    Inter('Inter-Regular', 400),
-    Inter('Inter-Medium', 500),
-    Inter('Inter-SemiBold', 600),
-    Inter('Inter-Bold', 700),
-    Inter('Inter-ExtraBold', 800),
-    Inter('Inter-Italic', 400, 'italic'),
+    Roobert('Roobert-Regular', 400),
+    Roobert('Roobert-Medium', 500),
+    Roobert('Roobert-SemiBold', 600),
+    Roobert('Roobert-Bold', 700),
+    Roobert('Roobert-ExtraBold', 800),
+    Roobert('Roobert-Italic', 400, 'italic'),
 
-    PolySans('PolySans-Slim', 300),
-    PolySans('PolySans-Neutral', 400),
-    PolySans('PolySans-Median', 500),
-    PolySans('PolySans-Median', 600),
-    PolySans('PolySans-Median', 700),
-    PolySans('PolySans-Median', 800),
-
-    JetBrains('JetBrainsMono-Regular', 400),
-    JetBrains('JetBrainsMono-Medium', 500),
-    JetBrains('JetBrainsMono-SemiBold', 600),
-    JetBrains('JetBrainsMono-Bold', 700),
+    GeistMono('GeistMono-Regular', 400),
+    GeistMono('GeistMono-Medium', 500),
+    GeistMono('GeistMono-SemiBold', 600),
+    GeistMono('GeistMono-Bold', 700),
   ],
 })
