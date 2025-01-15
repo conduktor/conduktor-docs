7e2f9bface8628243dacef3e0a5dfe6e6391cd1c
Claudiu Sergiu Danaila <csdmedianet@gmail.com>
2025-01-15 16:13:26 +0000

    Font update + a few style changes

--- src/features/home/organisms/BrowseBy/BrowseBy.constants.ts
+++ src/features/home/organisms/BrowseBy/BrowseBy.constants.ts
@@ -4,36 +4,47 @@ export const products = [
     icon: '/assets/svgs/guides/scale.svg',
     description: 'Scale your stuff',
     items: [
-      [ 'Configuration', '/platform/get-started/configuration/env-variables' ],
-      [ 'RBAC', '/platform/navigation/settings/rbac' ],
-      [ 'Self-service Quickstart', 'https://docs.conduktor.io/platform/guides/self-service-quickstart/' ],
-      [ 'Kafka Resource Management', 'https://docs.conduktor.io/platform/navigation/console/' ],
-      [ 'Monitoring & Alerting', 'https://docs.conduktor.io/platform/navigation/monitoring/' ],
-      [ 'Traffic Control Policies', 'https://docs.conduktor.io/gateway/category/traffic-control-policies/' ],
-    ]    
+      ['Configuration', '/platform/get-started/configuration/env-variables'],
+      ['RBAC', '/platform/navigation/settings/rbac'],
+      [
+        'Self-service Quickstart',
+        'https://docs.conduktor.io/platform/guides/self-service-quickstart/',
+      ],
+      ['Kafka Resource Management', 'https://docs.conduktor.io/platform/navigation/console/'],
+      ['Monitoring & Alerting', 'https://docs.conduktor.io/platform/navigation/monitoring/'],
+      [
+        'Traffic Control Policies',
+        'https://docs.conduktor.io/gateway/category/traffic-control-policies/',
+      ],
+    ],
   },
   {
     title: 'Shield',
     icon: '/assets/svgs/guides/shield.svg',
     description: 'Shield your stuff',
     items: [
-      [ 'Encryption', 'https://docs.conduktor.io/gateway/category/encryption/' ],
-      [ 'Audit', 'https://docs.conduktor.io/gateway/interceptors/data-security/audit/' ],
-      [ 'Data Masking', 'https://docs.conduktor.io/gateway/interceptors/data-security/data-masking/' ],
-      [ 'Dynamic Header Injection', 'https://docs.conduktor.io/gateway/interceptors/data-security/dynamic-header-injection/' ],
-    ]
+      ['Encryption', 'https://docs.conduktor.io/gateway/category/encryption/'],
+      ['Audit', 'https://docs.conduktor.io/gateway/interceptors/data-security/audit/'],
+      [
+        'Data Masking',
+        'https://docs.conduktor.io/gateway/interceptors/data-security/data-masking/',
+      ],
+      [
+        'Dynamic Header Injection',
+        'https://docs.conduktor.io/gateway/interceptors/data-security/dynamic-header-injection/',
+      ],
+    ],
   },
   {
     title: 'Exchange',
     icon: '/assets/svgs/guides/exchange.svg',
     description: 'Exchange your stuff',
     items: [
-      [ 'Third-Party Data Sharing', 'https://docs.conduktor.io/platform/data-sharing' ],
-      [ 'Partner Zones', 'https://docs.conduktor.io/platform/concepts/partner-zones/' ]
-    ]
+      ['Third-Party Data Sharing', 'https://docs.conduktor.io/platform/data-sharing'],
+      ['Partner Zones', 'https://docs.conduktor.io/platform/concepts/partner-zones/'],
+    ],
   },
 ]
-
 
 export const useCases = [
   {
@@ -41,12 +52,18 @@ export const useCases = [
     icon: '/assets/svgs/guides/use-cases.svg',
     description: 'Use cases for Conduktor',
     items: [
-      [ 'Configure SQL', 'https://docs.conduktor.io/platform/guides/configure-sql/' ],
-      [ 'Configure Chargeback', 'https://docs.conduktor.io/platform/guides/configure-chargeback/' ],
-      [ 'Configure Exportable Audit Log', 'https://docs.conduktor.io/platform/guides/configure-audit-log-topic/' ],
-      [ 'Configure Custom Deserializers', 'https://docs.conduktor.io/platform/guides/custom-deserializers/' ],
-      [ 'Configure SNI Routing', 'https://docs.conduktor.io/gateway/how-to/sni-routing/' ],
-      [ 'Configure Failover', 'https://docs.conduktor.io/gateway/how-to/configuring-failover/' ],
-    ]
-  }
+      ['Configure SQL', 'https://docs.conduktor.io/platform/guides/configure-sql/'],
+      ['Configure Chargeback', 'https://docs.conduktor.io/platform/guides/configure-chargeback/'],
+      [
+        'Configure Exportable Audit Log',
+        'https://docs.conduktor.io/platform/guides/configure-audit-log-topic/',
+      ],
+      [
+        'Configure Custom Deserializers',
+        'https://docs.conduktor.io/platform/guides/custom-deserializers/',
+      ],
+      ['Configure SNI Routing', 'https://docs.conduktor.io/gateway/how-to/sni-routing/'],
+      ['Configure Failover', 'https://docs.conduktor.io/gateway/how-to/configuring-failover/'],
+    ],
+  },
 ]
--- src/features/home/organisms/Hero/Hero.tsx
+++ src/features/home/organisms/Hero/Hero.tsx
@@ -11,19 +11,22 @@ interface HeroProps {}
 const Hero: React.FunctionComponent<HeroProps> = () => {
   return (
     <section className={styles.StyledHero}>
-        <div className={styles.ContentContainer}>
-          <Container>
-            <div className={styles.HeroContent}>
-              <p className={styles.HeroContentTitle}>Get Started with Conduktor</p>
-              <p>Learn how to install Conduktor and explore our Enterprise Data Management Platform For Streaming.</p>
-              <div className={styles.ButtonContainer}>
-                <Button to="/platform/category/get-started" type="primary" hasArrow>
-                  Get Conduktor
-                </Button>
-              </div>
+      <div className={styles.ContentContainer}>
+        <Container>
+          <div className={styles.HeroContent}>
+            <p className={styles.HeroContentTitle}>Get Started with Conduktor</p>
+            <p className={styles.Paragraph}>
+              Learn how to install Conduktor and explore our Enterprise Data Management Platform For
+              Streaming.
+            </p>
+            <div className={styles.ButtonContainer}>
+              <Button to="/platform/category/get-started" type="primary" hasArrow>
+                Get Started
+              </Button>
             </div>
-          </Container>
-        </div>
+          </div>
+        </Container>
+      </div>
     </section>
   )
 }
