e86d47a8df9a18bbdb00b3bf40661536ee19ced9
stu-conduktor <smould@conduktor.io>
2025-01-16 11:17:34 +0000

    Remove Intercom from doc site

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,7 +1,6 @@
 import Head from '@docusaurus/Head'
 import Layout from '@theme-original/Layout'
 import React from 'react'
-import { IntercomProvider } from 'react-use-intercom'
 
 export default function LayoutWrapper(props) {
   return (
@@ -9,9 +8,7 @@ export default function LayoutWrapper(props) {
       <Head>
         <meta property="og:image" content="/assets/images/og.png" />
       </Head>
-      <IntercomProvider appId={process.env.REACT_APP_INTERCOM_ID} autoBoot={true}>
-        <Layout {...props} />
-      </IntercomProvider>
+      <Layout {...props} />
     </>
   )
 }
