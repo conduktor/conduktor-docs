2a8cb7e02e2e28edbfee51ebaafd958546e817e3
Vadym <unclevader@gmail.com>
2025-01-20 20:47:48 +0000

    home page done

--- src/features/home/organisms/BrowseByProducts/BrowseByProducts.constants.ts
+++ src/features/home/organisms/BrowseByProducts/BrowseByProducts.constants.ts
@@ -3,32 +3,32 @@ export const products = [
     title: 'Scale',
     description: 'Scale your stuff',
     items: [
-      [ 'Configuration', '/platform/get-started/configuration/env-variables', '/assets/svgs/guides/configuration.svg' ],
-      [ 'RBAC', '/platform/navigation/settings/rbac' ],
-      [ 'Self-service Quickstart', 'https://docs.conduktor.io/platform/guides/self-service-quickstart/' ],
-      [ 'Kafka Resource Management', 'https://docs.conduktor.io/platform/navigation/console/' ],
-      [ 'Monitoring & Alerting', 'https://docs.conduktor.io/platform/navigation/monitoring/' ],
-      [ 'Traffic Control Policies', 'https://docs.conduktor.io/gateway/category/traffic-control-policies/' ],
+      [ 'Configuration', '/platform/get-started/configuration/env-variables', '/assets/svgs/products/configuration.svg' ],
+      [ 'RBAC', '/platform/navigation/settings/rbac', '/assets/svgs/products/rbac.svg'  ],
+      [ 'Self-service Quickstart', 'https://docs.conduktor.io/platform/products/self-service-quickstart/', '/assets/svgs/products/self-service-quickstart.svg'  ],
+      [ 'Kafka Resource Management', 'https://docs.conduktor.io/platform/navigation/console/', '/assets/svgs/products/kafka-resource-management.svg'  ],
+      [ 'Monitoring & Alerting', 'https://docs.conduktor.io/platform/navigation/monitoring/', '/assets/svgs/products/monitoring-and-alerting.svg'  ],
+      [ 'Traffic Control Policies', 'https://docs.conduktor.io/gateway/category/traffic-control-policies/', '/assets/svgs/products/traffic.svg'  ],
     ]    
   },
   {
     title: 'Shield',
-    icon: '/assets/svgs/guides/shield.svg',
+    icon: '/assets/svgs/products/shield.svg',
     description: 'Shield your stuff',
     items: [
-      [ 'Encryption', 'https://docs.conduktor.io/gateway/category/encryption/' ],
-      [ 'Audit', 'https://docs.conduktor.io/gateway/interceptors/data-security/audit/' ],
-      [ 'Data Masking', 'https://docs.conduktor.io/gateway/interceptors/data-security/data-masking/' ],
-      [ 'Dynamic Header Injection', 'https://docs.conduktor.io/gateway/interceptors/data-security/dynamic-header-injection/' ],
+      [ 'Encryption', 'https://docs.conduktor.io/gateway/category/encryption/', '/assets/svgs/products/rbac.svg'  ],
+      [ 'Audit', 'https://docs.conduktor.io/gateway/interceptors/data-security/audit/', '/assets/svgs/products/audit.svg'  ],
+      [ 'Data Masking', 'https://docs.conduktor.io/gateway/interceptors/data-security/data-masking/', '/assets/svgs/products/data-masking.svg'  ],
+      [ 'Dynamic Header Injection', 'https://docs.conduktor.io/gateway/interceptors/data-security/dynamic-header-injection/', '/assets/svgs/products/dynamic.svg'  ],
     ]
   },
   {
     title: 'Exchange',
-    icon: '/assets/svgs/guides/exchange.svg',
+    icon: '/assets/svgs/products/exchange.svg',
     description: 'Exchange your stuff',
     items: [
-      [ 'Third-Party Data Sharing', 'https://docs.conduktor.io/platform/data-sharing' ],
-      [ 'Partner Zones', 'https://docs.conduktor.io/platform/concepts/partner-zones/' ]
+      [ 'Third-Party Data Sharing', 'https://docs.conduktor.io/platform/data-sharing', '/assets/svgs/products/data-sharing.svg'  ],
+      [ 'Partner Zones', 'https://docs.conduktor.io/platform/concepts/partner-zones/', '/assets/svgs/products/partner-zones.svg'  ]
     ]
   },
 ]
